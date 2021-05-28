94a93d26fc231210e5b6acbd861015beafcbfca4
Friedrich Kräft <fraftdev@outlook.de>
2021-05-28 02:37:13 +0200

    Projektdateien hinzufügen.

---
+++ CoinMoin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoinMoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
